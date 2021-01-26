0cd07369a8f90983343dc7b81a17a8579825a086
pbebla <pauldinb@gmail.com>
2021-01-25 18:30:54 -0800

    added SRT algorithm

--- project-2/main.cpp
+++ project-2/main.cpp
@@ -22,6 +22,7 @@
 #include "stats.hpp"
 #include "hpfnp.hpp"
 #include "roundRobin.hpp"
+#include "srt.hpp"
 
 using namespace std;
 
@@ -31,11 +32,9 @@ const int WORKLOAD = 5;
 // RR demo func declaration
 void round_robin_demo();
 
-
 //Generate a Random Job
 //Paramas: a->the List working with, startpoint -> where you want to start,
 //count -> how many elements
-
 
 int generateJob(List &a, int startpoint, int count)
 {
@@ -43,80 +42,71 @@ int generateJob(List &a, int startpoint, int count)
     Job jobs;
     int arr, serv, pri;
     //generate the random values for a job.
-    for(i = startpoint;  i <= startpoint + count - 1; ++i)
+    for (i = startpoint; i <= startpoint + count - 1; ++i)
     {
         string n = "P" + to_string(i);
-        arr = rand() %100;
+        arr = rand() % 100;
         serv = (rand() % 11) + 1;
         pri = (rand() % 4) + 1;
-        jobs=Job(arr, serv, pri,n);
+        jobs = Job(arr, serv, pri, n);
         a.insertData(jobs);
     }
     return i;
 }
 
-
 int main()
 {
-	cout << "Note: this is C++" <<endl;
-	List yay;
-	int seed = 1;
-	int count, count2;
-	srand(time(NULL));
-	yay = List();
-	//generate 10 jobs
-	count = generateJob(yay,1,10);
-	count2 = 5;
-	//generatate more jobs if needed
-	while(yay.notIdle())
-	{
-		count = generateJob(yay,count,count2);
-		count2 += 5;
-	}
-	cout <<"Starting # of Jobs: " << yay.length() << endl;
-	//run algo
-	hpfnp(yay);
-  FCFS(&yay);     // First Come First Serve
-	yay.clr();
-	
-  // Round Robin Demo
-  round_robin_demo();
-    
+    cout << "Note: this is C++" << endl;
+    List yay;
+    int seed = 1;
+    int count, count2;
+    srand(time(NULL));
+    yay = List();
+    //generate 10 jobs
+    count = generateJob(yay, 1, 10);
+    count2 = 5;
+    //generatate more jobs if needed
+    while (yay.notIdle())
+    {
+        count = generateJob(yay, count, count2);
+        count2 += 5;
+    }
+    cout << "Starting # of Jobs: " << yay.length() << endl;
+    //run algo
+    //hpfnp(yay);
+    //FCFS(&yay); // First Come First Serve
+    srt(yay);
+    yay.clr();
 
-    
-   cout << "end of program" <<endl;
-   return 0;
+    // Round Robin Demo
+    //round_robin_demo();
 
-    
+    cout << "end of program" << endl;
+    return 0;
 }
 
 void round_robin_demo()
 {
     for (int i = 0; i < WORKLOAD; i++)
     {
-        cout << "*** Round Robin run: " << i+1 << " ***" << endl;
+        cout << "*** Round Robin run: " << i + 1 << " ***" << endl;
         List yay;
         time_t seed = time(NULL);
         int count, count2;
         srand(seed);
         yay = List();
         //generate 10 jobs
-        count = generateJob(yay,1,10);
+        count = generateJob(yay, 1, 10);
         count2 = 2;
         //generatate more jobs if needed
-        while(yay.notIdle())
+        while (yay.notIdle())
         {
-            count = generateJob(yay,count,count2);
+            count = generateJob(yay, count, count2);
             count2 *= 2;
-        }        
+        }
         // Round-Robin Scheduler
         round_robin_scheduler(&yay);
-        
+
         yay.clr();
     }
 }
-
-
-
-
-
