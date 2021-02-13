f38d8e5d4271b23a79b080f3f2f7c4c7f033a35a
Quan Bach <qbach@scu.edu>
2021-02-12 19:24:41 -0800

    add locality reference function to compute the next reference page

---
+++ project-4/utils.hpp
@@ -0,0 +1,24 @@
+/*
+ 
+ Santa Clara University
+ COEN 383 - Ad. Operating Systems
+ Winter 2021
+ Group 5
+    Anh Truong
+    Pauldin Bebla
+    Quan Bach
+    Travis Le
+    Yukun Zhang
+ 
+ Project 4
+ 
+ */
+
+#ifndef utils_hpp
+#define utils_hpp
+
+#include <cmath>
+
+int locality_reference(int reference_page, int num_of_pages);
+
+#endif /* utils_hpp */
