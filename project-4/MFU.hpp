1e7a7d94fc2fe762eadcb869498039f42970c131
yukunzhang115 <77864795+yukunzhang115@users.noreply.github.com>
2021-02-15 00:57:56 -0800

    Update MFU.hpp

--- project-4/MFU.hpp
+++ project-4/MFU.hpp
@@ -13,8 +13,7 @@
 #include "utils.hpp"
 
 size_t MFU(CustomQueue myQueue, double &rate);
-void pMFU(Memory &m, std::vector<Job *> &jobs, Job *insertLoc, int num, int timestamp);
-void MFUpushMore(Memory &m, Job * process);
+// void MFUpushMore(Memory &m, Job * process);
 void MFUprintTimeStamp(Memory &m, Job * process, int timestamp, std::string in);
 void MFUprintTimeStampMS(std::string cProc, int cPage, int timestamp, int PageInMem, std::string rProc, int Page_TBE);
 
