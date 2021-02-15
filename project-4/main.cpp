890fbe2d41050c3e85d4d6f01c50b676907f7fa5
Travis <travisl22@gmail.com>
2021-02-14 22:43:44 -0800

    Added Some more ouput things

--- project-4/main.cpp
+++ project-4/main.cpp
@@ -27,7 +27,7 @@ int main(int argc, const char * argv[])
 {
 	srand(time(0));
     int n;
-    double a;
+    double a, b, a2, b2;
     CustomQueue q;
     q.generateProcesses();
     //Job a = q.popProcess();
@@ -36,8 +36,28 @@ int main(int argc, const char * argv[])
     //cout << a.isListed(1) << endl;
     //a.removePage(1);
     //cout << a.isListed(1) << endl;
-    
-    LRU(q, a);
+    for (int i =0; i < 5 ; ++i)
+    {	
+    	double c;
+    	CustomQueue q;
+    	q.generateProcesses();
+    	std::cout<<"-----------60 seconds--------------" <<std::endl;
+    	a2+= LRU(q, c, true);
+    	a+= c;
+    	std::cout<<std::endl;
+    	std::cout<<"---------------100 References----------" <<std::endl;
+    	b2= LRU(q, c, false);
+    	b+= c;
+    	std::cout<<std::endl;
+    }
+    std::cout <<"LRU avg # of hit/miss of 60s: "
+    << a/5.0<< std::endl;
+    std::cout <<"LRU avg # of hit/miss of 100 References: "
+    << b/5.0 <<std::endl;
+    std::cout <<"LRU avg # of processes switched of 60s: "
+    << a2/5.0<<std::endl;
+    std::cout <<"LRU avg # of processes of 100 References: "
+    << b2/5.0<<std::endl;
     /*
     if(argc !=2)
     {
