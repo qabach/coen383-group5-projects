15c6067b0ab37c51a2fe9114734a45f590a77ec3
pbebla <pauldinb@gmail.com>
2021-02-14 23:40:47 -0800

    initial commit

---
+++ project-4/random.hpp
@@ -0,0 +1,16 @@
+//
+//  random.hpp
+//  COEN383
+//
+//  Created by Pauldin Bebla on 2/14/21.
+//
+
+#ifndef random_hpp
+#define random_hpp
+
+#include <stdio.h>
+#include "memory.hpp"
+#include "utils.hpp"
+#include "linked.hpp"
+void random(CustomQueue queue);
+#endif /* random_hpp */
