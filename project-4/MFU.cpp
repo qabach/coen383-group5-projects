0ab5ec61c4acc1ddbec334f23cda6a151db3099f
yukunzhang115 <77864795+yukunzhang115@users.noreply.github.com>
2021-02-14 21:41:07 -0800

    Update MFU.cpp

--- project-4/MFU.cpp
+++ project-4/MFU.cpp
@@ -9,62 +9,70 @@
 
 void pMFU(Memory &m, std::vector<Job> &jobs, int &insertLoc, int num);
 
-void MFU(CustomQueue myQueue)
-{
-//    CustomQueue myQueue;
-//    queue.
+void MFU(CustomQueue queue) {
+	std::cout << std::endl <<"************************* MFU ****************************" << std::endl;
     
-    
-    std::cout << std::endl <<"************************* LRU ****************************" << std::endl;
-    
-    // check memory free page size
-    Memory myMem;
+    Memory memory = Memory();
     size_t hit = 0, miss = 0;
-    myMem = Memory();
     
-    int freeMemSize = myMem.getFreeMemNum();
+    int freeMemorySize = memory.getFreeMemNum();
+	std::cout << "freeMemorySize: " << freeMemorySize << std::endl;
     
-    std::vector<Job> inMem;
-    std::vector<int> lastAccessed;
+    std::vector<Job> inMemoryJobList;
+    std::vector<int> lastAccessedJobList;
     
-    std::cout << "freeMemSize: " << freeMemSize << std::endl;
-    //please put in memory until full?
-	if (freeMemSize >= 4)
-	{
-		Job process = myQueue.popProcess();
-		int memLoc = myMem.getFreePage(myMem);
-		std::cout << "memLoc: " << memLoc << std::endl;
-		myMem.insertPageToMem(process, 0, memLoc);
-		inMem.push_back(process);
-		inMem.back().insertPage(0,memLoc);
-		lastAccessed.push_back(0);
-		++miss;
+	// if (freeMemorySize >= 4){
+	// 	Job job = queue.popProcess();
+
+	// 	int memoryLocation = memory.getFreePage();
+	// 	std::cout << "memoryLocation: " << memoryLocation << std::endl;
+
+	// 	memory.insertPageToMem(job, memoryLocation);
+	// 	inMemoryJobList.push_back(job);
+	// 	inMemoryJobList.back().insertPage(0, memoryLocation);
+	// 	lastAccessed.push_back(0);
+
+	// 	++miss;
 		    
-	}
-    for(int globalTime = 0; globalTime < 60; ++globalTime)
-    {
-		
-		//the 10ms that happens
-		for(int i =0 ; i < 100; ++i)
-		{
-			for(std::vector<Job>::iterator k = inMem.begin(); k != inMem.end(); ++k)
+	// }
+
+	
+	std::vector<Job>::iterator arrivedJobIterator = queue.processes.begin();
+	std::deque<Job> arrivedJobList;
+
+    for(float globalTime = 0; globalTime < 60; globalTime = globalTime + 0.1) {
+
+		while(arrivedJobIterator != queue.processes.end() && (*arrivedJobIterator).arrivelTime <= globalTime) {
+			arrivedJobList.push_back(*arrivedJobIterator);
+			arrivedJobIterator++;
+		}
+
+		if(memory.getFreeMemNum() >= 4 && !arrivedJobList.empty()) {
+			Job job = arrivedJobList.pop_front();
+			int memoryLocation = memory.getFreePage();
+			memory.insertPageToMem(job, memoryLocation);
+			inMemoryJobList.push_back(job);
+			inMemoryJobList.back().insertPage(0, memoryLocation);
+			lastAccessedJobList.push_back(0);
+		}
+
+		for(std::vector<Job>::iterator jobIterator = inMemoryJobList.begin(); jobIterator != inMemoryJobList.end(); ++jobIterator)
 			{
 				//for each job find the locality reference
-				int pos = k - inMem.begin();
-				int freeMemSize = myMem.getFreeMemNum();
-				std::cout << lastAccessed[pos] << " " <<k->size<<std::endl;
-				lastAccessed[pos] = locality_reference(
-					lastAccessed[pos], k->size);
+				int position = jobIterator - inMemoryJobList.begin();
+				int freeMemSize = memory.getFreeMemNum();
+				// lastAccessedJobList[position] = locality_reference(lastAccessedJobList[position], k->size);
+				int nextPageNumber = locality_reference(lastAccessedJobList[position], freeMemSize);
+
 				//if its listed reset timer of last accessed
-				if(k->isListed(pos)){
+				if(k->isListed(pos)) {
 					++hit;
-					k->resetTime(pos);
+					// k->resetTime(pos);
 					continue;
 				}
 				//if if not and size not free perform LRU
-				else if(freeMemSize <=0)
-				{
-					pLRU(myMem, inMem, pos, lastAccessed[pos]);
+				else if(freeMemSize <= 0) {
+					pMFU(memory, inMemoryJobList, position, nextPageNumber);
 				}
 				//just insert page inside memoryMap if found something free
 				else
